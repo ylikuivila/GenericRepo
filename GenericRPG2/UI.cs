1e9713cbe83dee826d3f983caf419e18f141490a
YKSkool <ylikuivila@gmail.com>
2015-11-05 10:48:19 +0200

    TestiMaster1

---
+++ GenericRPG2/UI.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace GenericRPG2
+{
+    class UI
+    {
+
+        internal void showText(string txt)
+        {
+            Console.WriteLine(txt);
+        }
+
+
+        internal string getUserInput()
+        {
+            return Console.ReadLine();
+        }
+
+
+
+
+
+
+
+
+
+
+    }
+}
